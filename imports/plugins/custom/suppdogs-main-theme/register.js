c262529470e40ef6b1f81dad8a9098efda49b4da
jameseffarah <jameseffarah@bia.agency>
2017-09-08 17:07:50 -0600

    added supdogs-main-theme

---
+++ imports/plugins/custom/suppdogs-main-theme/register.js
@@ -0,0 +1,9 @@
+import { Reaction } from "/server/api";
+
+Reaction.registerPackage({
+  // Name of the theme for presentation purposes
+  label: "suppDogs Theme",
+
+  // Name of your theme to uniquely identify it from other themes
+  name: "suppdogs-theme"
+});
